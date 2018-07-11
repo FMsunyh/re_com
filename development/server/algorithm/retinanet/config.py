be9e121c00f39eca3bed7db9a981672d2b43b71f
fmsunyh <fmsunyh@gmail.com>
2018-07-11 10:52:31 +0800

    initialize release version

--- development/server/algorithm/retinanet/config.py
+++ development/server/algorithm/retinanet/config.py
@@ -4,4 +4,4 @@ from server.config import ROOT_DIR,WEIGHT_PATH
 
 LABEL_MAPPING_PATH =os.path.join(ROOT_DIR, 'server/algorithm/retinanet/mapping_all.json')
 MODEL_PATH = WEIGHT_PATH
-LOAD_MODEL=False
+LOAD_MODEL=True
